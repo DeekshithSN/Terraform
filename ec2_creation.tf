4db785e64f7c7ec28ec4f47cd7d67b7fb53263c3
DeekshithSN <deekshithsn@gmail.com>
2019-01-22 12:28:05 +0530

    Create ec2_creation.tf

---
+++ ec2_creation.tf
@@ -0,0 +1,80 @@
+
+
+provider "aws" {
+	region = "us-west-2"
+	access_key = "*****"
+	secret_key = "*****"
+}
+
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags {
+    Name = "test-vpc"
+  }
+}
+
+resource "aws_subnet" "public-subnet" {
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+
+  tags {
+    Name = "Web Public Subnet"
+  }
+}
+
+resource "aws_security_group" "sgweb" {
+  name = "vpc_test_web"
+  description = "Allow incoming HTTP connections & SSH access"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks =  ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  vpc_id="${aws_vpc.default.id}"
+
+  tags {
+    Name = "Web Server SG"
+  }
+}
+
+resource "aws_instance" "web" {
+	ami = "ami-18726478"
+	instance_type = "t2.micro"
+	subnet_id = "${aws_subnet.public-subnet.id}"
+	vpc_security_group_ids = ["${aws_security_group.sgweb.id}"]
+}
+
