00f6f1eaca3847f07bba87a66bddad4ada20e4cc
DeekshithSN <deekshithsn@gmail.com>
2019-01-22 22:36:41 +0530

    Update admin.tf

--- IAM/admin.tf
+++ IAM/admin.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIAIA7H3ISGMBV4I7XA"
-  secret_key = "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"
+  access_key = "****"
+  secret_key = "****"
 }
 
 variable "user_name" {}
