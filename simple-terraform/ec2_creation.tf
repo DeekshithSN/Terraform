27ef26ec4e0b14427c640ca2cfbc22d5000d850b
Deekshith SN <deekshithsn@gmail.com>
2020-10-12 19:47:12 +0530

    Update ec2_creation.tf

--- simple-terraform/ec2_creation.tf
+++ simple-terraform/ec2_creation.tf
@@ -3,6 +3,14 @@ provider "aws" {
 	region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "terraform-backend-spovedd"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
