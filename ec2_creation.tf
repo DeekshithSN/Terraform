ea34c1fb41bfddb243e76f4384a3734341bcaca4
Deekshith SN <deekshithsn@gmail.com>
2020-10-12 17:39:54 +0530

    Update ec2_creation.tf

--- ec2_creation.tf
+++ ec2_creation.tf
@@ -10,9 +10,7 @@ resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
 
-  tags {
-    Name = "test-vpc"
-  }
+  
 }
 
 resource "aws_subnet" "public-subnet" {
