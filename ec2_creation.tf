9160b1ffc669b30d466d7947728cc9735a39a30f
Deekshith SN <deekshithsn@gmail.com>
2020-10-12 17:41:36 +0530

    Update ec2_creation.tf

--- ec2_creation.tf
+++ ec2_creation.tf
@@ -1,11 +1,5 @@
 
 
-provider "aws" {
-	region = "us-west-2"
-	access_key = "*****"
-	secret_key = "*****"
-}
-
 resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
