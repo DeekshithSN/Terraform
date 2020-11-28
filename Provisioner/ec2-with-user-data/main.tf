2718f602f378fafb29b5b1a0b778526e04c76473
EC2 Default User <ec2-user@ip-172-31-87-168.ec2.internal>
2020-11-28 14:59:24 +0000

    provision

---
+++ Provisioner/ec2-with-user-data/main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-04bf6dcdc9ab498ca"
+  instance_type = "t2.micro"
+  key_name      = "aws_iny_lappi"
+  user_data     = "${file("httpd.sh")}"
+  vpc_security_group_ids = ["${aws_security_group.webSG.id}"]
+  tags = {
+    Name = "Test-file-provisioner"
+  }
+}
+
+
+resource "aws_security_group" "webSG" {
+  name        = "webSG"
+  description = "Allow ssh  inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    
+  }
+}
