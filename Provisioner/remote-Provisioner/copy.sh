877a33b400bbfbdda64289d623bd417c7e3c63db
Deekshith SN <deekshithsn@gmail.com>
2020-11-29 13:40:44 +0530

    Update copy.sh

--- Provisioner/remote-Provisioner/copy.sh
+++ Provisioner/remote-Provisioner/copy.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
+UID=$(id -u)
+if [ x$UID != x0 ] 
+then
+    #Beware of how you compose the command
+    printf -v cmd_str '%q ' "$0" "$@"
+    exec sudo su -c "$cmd_str"
+fi
+
 sudo mv /tmp/index.html /var/www/html/
 sudo systemctl restart httpd
