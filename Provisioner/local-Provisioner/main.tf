5ba761866136d7f4ab36862c6d3a286cfdf792ad
Deekshith SN <deekshithsn@gmail.com>
2020-12-04 21:30:47 +0530

    Update main.tf

--- Provisioner/local-Provisioner/main.tf
+++ Provisioner/local-Provisioner/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "web" {
   ami           = "ami-04bf6dcdc9ab498ca"
   instance_type = "t2.micro"
   tags = {
-    Name = "Test-file-provisioner"
+    Name = "local-exec-provisioner"
   }
   
   provisioner "local-exec" {
